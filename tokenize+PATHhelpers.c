6bd4074ba2413be05b009aff89638603eec7f0c2
damukuzi <damukuzi@gmail.com>
2022-06-15 10:12:57 -0700

    adding changes

--- tokenize+PATHhelpers.c
+++ tokenize+PATHhelpers.c
@@ -7,26 +7,26 @@
  */
 char **tokenize(char *buffer)
 {
-    char *token;
-    int i = 0, wordcount = 0;
-    char *delimiter = " \n";
-    char **av;
+	char *token;
+	int i = 0, wordcount = 0;
+	char *delimiter = " \n";
+	char **av;
 
-    wordcount = _splitstring(buffer);
-    if (!wordcount)
-        return (NULL);
-    av = malloc((wordcount + 1) * sizeof(char *));
-    if (av == NULL)
-        exit(1);
-    token = strtok(buffer, delimiter);
-    while (token != NULL)
-    {
-        av[i] = _strdup(token);
-        token = strtok(NULL, delimiter);
-        i++;
-    }
-    av[i] = NULL;
-    return (av);
+	wordcount = _splitstring(buffer);
+	if (!wordcount)
+		return (NULL);
+	av = malloc((wordcount + 1) * sizeof(char *));
+	if (av == NULL)
+		exit(1);
+	token = strtok(buffer, delimiter);
+	while (token != NULL)
+	{
+		av[i] = _strdup(token);
+		token = strtok(NULL, delimiter);
+		i++;
+	}
+	av[i] = NULL;
+	return (av);
 }
 /**
  * _splitPATH - counts the number of PATH members
@@ -36,23 +36,23 @@ char **tokenize(char *buffer)
  */
 int _splitPATH(char *str)
 {
-    int i;
-    int searchflag = 1;
-    int wordcount = 0;
+	int i;
+	int searchflag = 1;
+	int wordcount = 0;
 
-    for (i = 0; str[i]; i++)
-    {
-        if (str[i] != ':' && searchflag == 1)
-        {
-            wordcount += 1;
-            searchflag = 0;
-        }
-        if (str[i + 1] == ':')
-        {
-            searchflag = 1;
-        }
-    }
-    return (wordcount);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ':' && searchflag == 1)
+		{
+			wordcount += 1;
+			searchflag = 0;
+		}
+		if (str[i + 1] == ':')
+		{
+			searchflag = 1;
+		}
+	}
+	return (wordcount);
 }
 /**
  * _PATHstrcmp - compares PATH with environ to find PATH value
@@ -63,14 +63,14 @@ int _splitPATH(char *str)
  */
 int _PATHstrcmp(const char *s1, const char *s2)
 {
-    int i;
+	int i;
 
-    for (i = 0; s2[i] != '='; i++)
-    {
-        if (s1[i] != s2[i])
-            return (-1);
-    }
-    return (0);
+	for (i = 0; s2[i] != '='; i++)
+	{
+		if (s1[i] != s2[i])
+			return (-1);
+	}
+	return (0);
 }
 /**
  * _concat - concats user string with PATH member string and /
@@ -82,13 +82,13 @@ int _PATHstrcmp(const char *s1, const char *s2)
  */
 char *_concat(char *tmp, char **av, char *tok)
 {
-    int len = 0;
+	int len = 0;
 
-    _memset(tmp, 0, 256);
-    len = _strlen(tok) + _strlen(av[0]) + 2;
-    _strcat(tmp, tok);
-    _strcat(tmp, "/");
-    _strcat(tmp, av[0]);
-    tmp[len - 1] = '\0';
-    return (tmp);
+	_memset(tmp, 0, 256);
+	len = _strlen(tok) + _strlen(av[0]) + 2;
+	_strcat(tmp, tok);
+	_strcat(tmp, "/");
+	_strcat(tmp, av[0]);
+	tmp[len - 1] = '\0';
+	return (tmp);
 }
